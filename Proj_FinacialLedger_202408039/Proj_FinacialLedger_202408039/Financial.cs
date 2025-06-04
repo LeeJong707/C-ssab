32914998272a33296a4e6d51c197c9b5ac3482ae
LeeJong707 <ljwomlj0403@gmail.com>
2025-06-04 15:21:34 +0900

    Add files via upload

---
+++ Proj_FinacialLedger_202408039/Proj_FinacialLedger_202408039/Financial.cs
@@ -0,0 +1,73 @@
+﻿using Proj_FinacialLedger_202408039;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj_FinacialLedger_202408039
+{
+    public abstract class Financial
+    {
+        protected Dictionary<DateTime, Info> _incomes = new Dictionary<DateTime, Info>();
+        protected Dictionary<DateTime, Info> _expenditures = new Dictionary<DateTime, Info>();
+
+        public int TotalIncome
+        {
+            get
+            {
+                int total = 0;
+                foreach (var item in _incomes.Values)
+                    total += item.Money;
+                return total;
+            }
+        }
+
+        public int TotalExpenditure
+        {
+            get
+            {
+                int total = 0;
+                foreach (var item in _expenditures)
+                {
+                    total = total + item.Value.Money;
+                }
+                return total;
+            }
+        }
+
+        public bool RegIncome(DateTime date, string content, int money)
+        {
+            if (money >= 0)
+            {
+                Info info = new Info(content, money);
+                _incomes[date] = info;
+                return true;
+            }
+            return false;
+        }
+
+        public bool RegIncome(string content, int money)
+        {
+            DateTime now = DateTime.Now;
+            return RegIncome(now, content, money);
+        }
+
+        public bool RegExpenditure(DateTime date, string content, int money)
+        {
+            if (money >= 0)
+            {
+                Info info = new Info(content, money);
+                _expenditures[date] = info;
+                return true;
+            }
+            return false;
+        }
+
+        public bool RegExpenditure(string content, int money)
+        {
+            DateTime now = DateTime.Now;
+            return RegExpenditure(now, content, money);
+        }
+    }
+}
---
+++ Proj_FinacialLedger_202408039/Proj_FinacialLedger_202408039/FinancialLedger.cs
@@ -0,0 +1,102 @@
+﻿using Proj_FinacialLedger_202408039;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj_FinacialLedger_12345678
+{
+    public class FinancialLedger : Financial
+    {
+        private int _year;
+        private int _targetAmount;
+
+        public int Year
+        {
+            get { return _year; }
+        }
+
+        public int TargetAmount
+        {
+            get { return _targetAmount; }
+        }
+
+        public bool IsBlack
+        {
+            get
+            {
+                int income = TotalIncome;
+                int expend = TotalExpenditure;
+                int result = income - expend;
+
+                if (_targetAmount == 0)
+                {
+                    if (result > 0)
+                        return true;
+                    else
+                        return false;
+                }
+                else
+                {
+                    if (result >= _targetAmount)
+                        return true;
+                    else
+                        return false;
+                }
+            }
+        }
+
+        // 생성자 1
+        public FinancialLedger(int year)
+        {
+            _year = year;
+            _targetAmount = 0;
+        }
+
+        // 생성자 2
+        public FinancialLedger(int year, int targetAmount)
+        {
+            _year = year;
+            _targetAmount = targetAmount;
+        }
+
+        // 수입 등록
+        public new bool RegIncome(DateTime date, string content, int money)
+        {
+            if (date.Year == _year)
+            {
+                return base.RegIncome(date, content, money);
+            }
+            return false;
+        }
+
+        public new bool RegIncome(string content, int money)
+        {
+            if (DateTime.Now.Year == _year)
+            {
+                return base.RegIncome(content, money);
+            }
+            return false;
+        }
+
+        // 지출 등록
+        public new bool RegExpenditure(DateTime date, string content, int money)
+        {
+            if (date.Year == _year)
+            {
+                return base.RegExpenditure(date, content, money);
+            }
+            return false;
+        }
+
+        public new bool RegExpenditure(string content, int money)
+        {
+            if (DateTime.Now.Year == _year)
+            {
+                return base.RegExpenditure(content, money);
+            }
+            return false;
+        }
+    }
+}
